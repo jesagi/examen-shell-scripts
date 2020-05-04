c322eb22b5639d9e0eb1df4ab46fba11a2628762
jesagi <jesagi@floridauniversitaria.es>
2020-05-04 19:15:59 +0200

    commit 19:15

--- script.sh
+++ script.sh
@@ -1,16 +1,22 @@
 n_hombre=0
 n_mujeres=0
 contador=0
-ciudad=`cat instructores.txt | awk '{print $4}'`
-genero=`cat instructores.txt | awk '{print $6}'`
-max=`cat instuctores.txt | wc -l`
+ciudad=`cat instructores.txt | head -${contador} | tail -1 | awk '{print $4}'`
+genero=`cat instructores.txt | head -${contador} | tail -1 | awk '{print $6}'`
+max=`cat instructores.txt | wc -l`
 while [ $contador -lt $max ]  
     contador=$(($contador+1))
-    if [ $1 = $ciudad ]; do
-        if [ $genero = 'Hombre' ]; do
+    if [ $1 = $ciudad ]; then
+        if [ $genero = 'Hombre' ]; then
              n_hombre=$(($n_hombre+1))
 	else
 	    n_mujeres=$(($n_mujeres+1))
 	fi
     fi
 done
+
+if [ $n_hombres -gt $n_mujeres ]; then
+    echo "Hay un mayor numero de hombres"
+else 
+echo "Hay un mayor numero de mujeres"
+fi
