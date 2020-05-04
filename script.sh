65e6ff8e652b3d5c2ae4a89fbea29947d8d96a7c
jesagi <jesagi@floridauniversitaria.es>
2020-05-04 19:00:47 +0200

    commit 19:00

--- script.sh
+++ script.sh
@@ -1,8 +1,16 @@
 n_hombre=0
 n_mujeres=0
-linea=
-
-for i in $(cat instructores.txt | awk '{print $6}');do
-    
-
+contador=0
+ciudad=`cat instructores.txt | awk '{print $4}'`
+genero=`cat instructores.txt | awk '{print $6}'`
+max=`cat instuctores.txt | wc -l`
+while [ $contador -lt $max ]  
+    contador=$(($contador+1))
+    if [ $1 = $ciudad ]; do
+        if [ $genero = 'Hombre' ]; do
+             n_hombre=$(($n_hombre+1))
+	else
+	    n_mujeres=$(($n_mujeres+1))
+	fi
+    fi
 done
